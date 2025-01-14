50688fe092e2b4215644430846abcc89427cce5c
Golannnnn <forburacc@protonmail.ch>
2025-01-14 13:56:54 +0100

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://golannnnn.github.io",
+  base: "url-shortening",
+});
